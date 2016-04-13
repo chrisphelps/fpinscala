fff484bbd47cb46895733ce380a1724980f94b7d
John Hungerford <jhungerford@gmail.com>
2016-04-13 06:59:15 -0600

    Exercise 3.2

--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -3,17 +3,28 @@ package fpinscala.datastructures
 import org.scalatest.{FlatSpec, Matchers}
 
 class ListTest extends FlatSpec with Matchers {
+
   behavior of "Exercise 3.1"
 
   it should "be 3, I believe" in {
-    val x = List(1, 2, 3, 4, 5) match {
-      case Cons(x, Cons(2, Cons(4, _))) => x
-      case Nil => 42
-      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-      case Cons(h, t) => h + List.sum(t)
-      case _ => 101
-    }
+    List.x shouldEqual 3
+  }
 
-    x shouldEqual 3
+  behavior of "Exercise 3.2"
+
+  it should "remove the first item of a list with length 4" in {
+    List.tail(List(1, 2, 3, 4)) shouldEqual List(2, 3, 4)
+  }
+
+  it should "remove the first item of a list with length 2" in {
+    List.tail(List(1, 2)) shouldEqual List(2)
+  }
+
+  it should "remove the first item of a list with length 1" in {
+    List.tail(List(1)) shouldEqual Nil
+  }
+
+  it should "not explode on a Nil list" in {
+    List.tail(Nil) shouldEqual Nil
   }
 }
