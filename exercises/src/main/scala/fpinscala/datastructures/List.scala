a2bb2e72fede1aeae081a0022681001573eeb0f9
John Hungerford <jhungerford@gmail.com>
2016-04-13 06:41:46 -0600

    Exercise 3.1

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -66,3 +66,17 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
+
+object Result {
+  def main(args: Array[String]) {
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+      case _ => 101
+    }
+
+    println(x)
+  }
+}
